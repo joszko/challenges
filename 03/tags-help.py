b18f90b5e21313f6a36920f9be52536dddc50363
joszko <joszko@gmail.com>
2018-02-24 00:01:01 +0100

    challenge 03

--- 03/tags-help.py
+++ 03/tags-help.py
@@ -36,17 +36,23 @@ def get_similarities(tags):
     Hint 1: compare each tag, use for in for, or product from itertools (already imported)
     Hint 2: use SequenceMatcher (imported) to calculate the similarity ratio
     Bonus: for performance gain compare the first char of each tag in pair and continue if not the same"""
-    pass
+    similar_tags = []
+    for word1, word2 in list(product(tags, tags)):
+        if word1 != word2:
+            ratio = SequenceMatcher(None, word1, word2).ratio()
+            if IDENTICAL > ratio > SIMILAR:
+                similar_tags.append([word1, word2])
+    return similar_tags
 
 
-# if __name__ == "__main__":
-#     tags = get_tags()
-#     top_tags = get_top_tags(tags)
-#     print('* Top {} tags:'.format(TOP_NUMBER))
-#     for tag, count in top_tags:
-#         print('{:<20} {}'.format(tag, count))
-#     similar_tags = dict(get_similarities(tags))
-#     print()
-#     print('* Similar tags:')
-#     for singular, plural in similar_tags.items():
-#         print('{:<20} {}'.format(singular, plural))
+if __name__ == "__main__":
+    tags = get_tags()
+    top_tags = get_top_tags(tags)
+    print('* Top {} tags:'.format(TOP_NUMBER))
+    for tag, count in top_tags:
+        print('{:<20} {}'.format(tag, count))
+    similar_tags = dict(get_similarities(tags))
+    print()
+    print('* Similar tags:')
+    for singular, plural in similar_tags.items():
+        print('{:<20} {}'.format(singular, plural))
